1d4a3dcf544379acad3acc21dda90d52ca022279
Brenno Ysrael M. L. Correia <brenno.maluf@gmail.com>
2024-05-30 19:05:49 -0400

    Novamente Refatorado

--- Criação de um WebApp em React.Js/vite-project/src/App.jsx
+++ Criação de um WebApp em React.Js/vite-project/src/App.jsx
@@ -33,7 +33,8 @@ const App = () => {
   return <div>
     <p>Nome: {dados.cliente}</p>
     <p>Idade: {dados.idade}</p>
-    <p>Situação: <span style={dados.ativa ? { color: 'green' } : { color: 'red' }}>{dados.ativa ? 'Ativa' : 'Inativa'}</span>
+    <p>Situação:{' '}
+      <span style={{ color: dados.ativa ? 'green' : 'red' }}>{dados.ativa ? 'Ativa' : 'Inativa'}</span>
     </p>
     <p>Total gasto: R$ {totalGasto.toFixed(2)}</p>
     {totalGasto > 10000 ? <p>Você está gastando muito.</p> : ''}
