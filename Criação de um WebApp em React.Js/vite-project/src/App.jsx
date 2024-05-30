44fa535eb66f4d31240d801ea55fe00be2a20fbf
Brenno Ysrael M. L. Correia <brenno.maluf@gmail.com>
2024-05-30 17:10:10 -0400

    Exercício JSX - uma das forma de se fazer

--- Criação de um WebApp em React.Js/vite-project/src/App.jsx
+++ Criação de um WebApp em React.Js/vite-project/src/App.jsx
@@ -1,36 +1,48 @@
+// Mostre os dados da aplicação, como aprensetado no vídeo
+// Não utilize CSS externo, use o style para mudar as cores
+// Se a situação estiver ativa pinte de verde, inativa vermelho
+// Se o gasto for maior que 10000 mostre uma mensagem
+const luana = {
+  cliente: 'Luana',
+  idade: 27,
+  compras: [
+    { nome: 'Notebook', preco: 'R$ 2500' },
+    { nome: 'Geladeira', preco: 'R$ 3000' },
+    { nome: 'Smartphone', preco: 'R$ 1500' },
+  ],
+  ativa: true,
+};
+
+const mario = {
+  cliente: 'Mario',
+  idade: 31,
+  compras: [
+    { nome: 'Notebook', preco: 'R$ 2500' },
+    { nome: 'Geladeira', preco: 'R$ 3000' },
+    { nome: 'Smartphone', preco: 'R$ 1500' },
+    { nome: 'Guitarra', preco: 'R$ 3500' },
+  ],
+  ativa: false,
+};
+
 const App = () => {
-  function meuNome() {
-    return 'André';
-  }
+  const dados = mario;
+  let totalGasto = 0;
 
-  function quadrado(x) {
-    return x * x;
-  }
+  return <div>
 
-  const carro = {
-    rodas: 4,
-    marca: 'Ford',
-  };
-
-  return (
-    <div>
-      <p>{'MINHA EMPRESA'.toLowerCase()}</p>
-      <p>{30 * 2}</p>
-      <p>{true}</p>
-      <p>{undefined}</p> {/* valores Undefined não são renderizados */}
-      <p>{(() => 'Função Executada')()}</p>
-      <p>{meuNome()}</p>
-      <p>{quadrado(2)}</p>
-      <p>{quadrado(2) === 4 ? 'Fórmula correta' : 'Fórmula incorreta'}</p>
-      <p>{quadrado(2) === 4 && 'Fórmula correta'}</p>
-      <p>{Date.now()}</p>
-      <p>{new Date().toISOString()}</p>
-      <p>{new Date().getFullYear()}</p>
-      <p>
-        Marca: {carro.marca}, Rodas: {carro.rodas}
-      </p>
-    </div>
-  );
+    <p>Nome: {dados.cliente}</p>
+    <p>Idade: {dados.idade}</p>
+    <p>Situação: <span style={dados.ativa ? { color: 'green' } : { color: 'red' }}>{dados.ativa ? 'Ativa' : 'Inativa'}</span>
+    </p>
+    <p>Total gasto: {dados.compras.map((valor) => {
+      let precos = valor.preco
+      let precosFormatados = +precos.replace('R$ ', '')
+      totalGasto = totalGasto + precosFormatados
+    })} R$ {totalGasto.toFixed(2)}</p>
+    {totalGasto > 10000 ? <p>Você está gastando muito.</p> : ''}
+  </div>;
 };
 
 export default App;
+
